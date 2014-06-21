02c7d44d9fd7c944842c717322aab77fdffcb782
Sujay Busam <sujay.busam@gmail.com>
2014-06-21 12:52:18 -0400

    Add user model, migrate db

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,4 @@ module ApplicationHelper
 			"#{base_title} | #{page_title}"
 		end
 	end
-end
+end 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe "UserPages" do
 
 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }  
-  subject { page } 
+	subject { page } 
 
-  describe "signup page" do
-  	before { visit signup_path }
+	describe "signup page" do
+		before { visit signup_path }
 
-  	it { should have_content('Sign up') }
-  	it { should have_title("#{base_title} | Sign up") }
-  end
+		it { should have_content('Sign up') }
+		it { should have_title("#{base_title} | Sign up") }
+	end
 
 end
