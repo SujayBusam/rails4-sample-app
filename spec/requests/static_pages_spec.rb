9e2eb5f4fe05643bf7cc90ae7269313e62594d89
Sujay Busam <sujay.busam@gmail.com>
2014-06-21 02:06:11 -0400

    Refactor testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,64 +3,36 @@ require 'spec_helper'
 describe "StaticPages" do
 
 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
 	# Strings in describe could be anything. Rspec doesn't
   describe "Home page" do
+    before { visit root_path }
     
-    it "should have the h1 'Sample App'" do 
-    	visit root_path
-    	expect(page).to have_selector('h1', :text => 'Sample App')
-    end
-
-     it '"should have the title "Ruby on Rails Tutorial Sample App"' do
-  		visit root_path
-  		expect(page).to have_title("#{base_title}")
-  	end
-
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_title('| Home')
-    end
+    it { should have_selector('h1', :text => 'Sample App') }
+    it { should have_title("#{base_title}") }
+    it { should_not have_title('| Home') }
   end
 
-
   describe "Help page" do
+    before { visit help_path }
 
-  	it "should have the h1 'Help'" do
-  		visit help_path
-  		expect(page).to have_selector('h1', :text => 'Help')
-  	end
-
-  	it 'should have the title "Ruby on Rails Tutorial Sample App | Help"' do
-  		visit help_path
-  		expect(page).to have_title("#{base_title} | Help")
-  	end
+    it { have_selector('h1', :text => 'Help') }
+    it { should have_title("#{base_title} | Help") }
   end
 
   describe "About page" do
+    before {visit about_path }
 
-  	it "should have the h1 'About Us'" do
-  		visit about_path
-  		expect(page).to have_selector('h1', :text => 'About Us')
-  	end
-
-  	it 'should have the title "Ruby on Rails Tutorial Sample App | About Us"'do
-  		visit about_path
-  		expect(page).to have_title("#{base_title} | About Us")
-  	end
+    it { should have_selector('h1', :text => 'About Us') }
+    it { should have_title("#{base_title} | About Us") }
   end
 
   describe "Contact page" do
+    before {visit contact_path }
 
-  	it "should have the h1 'Contact'" do
-  		visit contact_path
-  		expect(page).to have_selector('h1', :text => 'Contact')
-  	end
-
-  	it 'should have the title "Ruby on Rails Tutorial Sample App | Contact"' do
-  		visit contact_path
-  		expect(page).to have_title("#{base_title} | Contact")
-  	end
+    it { should have_selector('h1', :text => 'Contact') }
+    it { should have_title("#{base_title} | Contact") }
   end
-
+  
 end
