01a4a0044b61e6f2b39e5110fc2185db77c3c32e
Sujay Busam <sujay.busam@gmail.com>
2014-06-22 23:06:36 -0400

    Finish User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ActiveRecord::Base
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i
 
-	validates :name, presence: true, length: { maximum: 50 }
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+	validates :name, presence: true, 
+					 length:   { maximum: 50 }
+					 
+	validates :email, presence:   true, 
+					  format:     { with: VALID_EMAIL_REGEX },
 					  uniqueness: { case_sensitive: false }
+
+	has_secure_password
+
+	# Presence validations for password and its confirmation automatically
+	# added by has_secure_password
+	validates :password, length: { minimum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 
 describe User do
-  
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
 
-  subject { @user }
+	before do 
+		@user = User.new(name: "Example User", email: "user@example.com",
+			password: "foobar", password_confirmation: "foobar")
+	end
 
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
+	subject { @user }
 
-  it { should be_valid }
+	it { should respond_to(:name) }
+	it { should respond_to(:email) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:authenticate) }
 
+	it { should be_valid }
+
+  ###### NAME #######
   describe "when name is not present" do
   	before { @user.name = " " }
   	it { should_not be_valid }
@@ -22,30 +30,73 @@ describe User do
   end
 
   describe "when name is too long" do
-	before { @user.name = "a" * (50 + 1) }
-	it { should_not be_valid }
+  	before { @user.name = "a" * (50 + 1) }
+  	it { should_not be_valid }
   end
 
+  ###### EMAIL ########
   describe "when email format is invalid" do
   	it "should be invalid" do
   		addresses = %w[user@foo,com user_at_foo.org example.user@foo.
   			foo@bar_baz.com foo@bar+baz.com foo@bar..com]
 
-  		# Iterate through invalid addresses
-  		addresses.each do |invalid_address|
-  			@user.email = invalid_address
-  			expect(@user).not_to be_valid
+  			addresses.each do |invalid_address|
+  				@user.email = invalid_address
+  				expect(@user).not_to be_valid
+  			end
   		end
   	end
-  end
 
-  describe "when email address is already taken" do
-  	before do
-  		duplicate_user = @user.dup
-  		duplicate_user.email = @user.email.upcase
-  		duplicate_user.save
+  	describe "when email address is already taken" do
+  		before do
+  			duplicate_user = @user.dup
+  			duplicate_user.email = @user.email.upcase
+  			duplicate_user.save
+  		end
+
+  		it { should_not be_valid }
   	end
 
+
+  ####### PASSWORD ########
+  describe "when a password is too short" do
+  	before { @user.password = @user.password_confirmation = ("a" * 5) }
+  	it { should be_invalid }
+  end
+
+  describe "when password is not present" do
+  	before do
+  		@user = User.new(name: "Example User", email: "user@example.com",
+  			password: " ", password_confirmation: " ")
+  	end
   	it { should_not be_valid }
   end
+
+  describe "when password doesn't match confirmation" do
+  	before { @user.password_confirmation = "mismatch" }
+  	it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+  	before { @user.save }
+  	# Let is memoizing. Only retrives from database once
+  	let(:found_user) { User.find_by(email: @user.email) }
+  	
+  	describe "with valid password" do
+  		it { should eq  found_user.authenticate(@user.password) }
+  	end
+
+  	describe "with invalid password" do
+  		let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+  		it { should_not eq user_for_invalid_password }
+  		specify { expect(user_for_invalid_password).to be_false }
+  	end
+  end
 end
+
+
+
+
+
+
