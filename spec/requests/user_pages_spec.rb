d34542e4a53247d9b06a865a56a0d2d4f06169dc
Sujay Busam <sujay.busam@gmail.com>
2014-06-24 15:49:58 -0400

    Minor sign-up cleanup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,6 +31,15 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+
+        # can add more specific error tests here
+      end
     end
 
     describe "with valid information" do
