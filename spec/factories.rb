6836d0c87437e82425a8e2d5689e491c8d10669a
Sujay Busam <sujay.busam@gmail.com>
2014-06-27 00:51:21 -0400

    Sign in complete

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do 
 	factory :user do
-		name "Sujay Busam"
-		email "sujaybusam@gmail.com"
+		name "Example User"
+		email "user@example.com"
 		password "foobar"
 		password_confirmation "foobar"
 	end
