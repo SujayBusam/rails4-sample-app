f4963b480678c26795e9851afcd598ae459c75e5
Sujay Busam <sujay.busam@gmail.com>
2014-07-01 00:05:51 -0400

    User edit and index complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,9 +11,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: user_email.downcase)
 
     if user && user.authenticate(user_password)
-      # Sign the user in and redirect to user's show page
+      # Sign the user in and redirect back or to user's show page
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       # Create an error message and re-render the signin form
       flash.now[:error] = 'Invalid email/password combination'
@@ -22,5 +22,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    sign_out
+    redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,6 +14,13 @@ module SessionsHelper
     self.current_user = user
   end
 
+  def sign_out
+    current_user.update_attribute(:remember_token,
+      User.digest(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
   def signed_in?
     !current_user.nil?
   end
@@ -26,5 +33,18 @@ module SessionsHelper
     remember_token = User.digest(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
     # return @current_user
+  end
+
+  def current_user?(user)
+    current_user == user
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
   end
 end
